ab6cabce968059e237cc56dd3aea25da43ffc2bf
BT_1N3_10 <BT_1N3_10@BT_1N3_10.bktech.local>
2017-12-13 13:09:23 -0500

    Started writing code

---
+++ ListRemoveDupes/src/removeDupes.java
@@ -0,0 +1,13 @@
+import java.util.List;
+public class removeDupes
+{
+	public static void main(String args[])
+	{
+		List<Integer> numList = new List<Integer>();
+		List.add(1);
+	}
+	public static List<Integer> removeDupes(List<Integer> nums)
+	{
+		
+	}
+}
